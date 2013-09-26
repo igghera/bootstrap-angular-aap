eb934f9d832985749e4a9a0dab37d25816032c23
Andrea Gherardi <andrea@panthacorp.com>
2013-09-26 10:43:07 +1000

    First commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,7 @@
+'use strict';
+
+angular.module('aapApp').controller('MainCtrl', function ($scope) {
+	$scope.dynamicPopover = "Hello, World!";
+	$scope.dynamicPopoverText = "dynamic";
+	$scope.dynamicPopoverTitle = "Title";
+});
