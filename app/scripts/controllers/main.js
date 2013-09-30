29a339b235221733aab86402094a191d1b3dd88c
Per Rosen <per@panthacorp.com>
2013-09-30 11:24:19 +1000

    Footer etc

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -5,3 +5,9 @@ angular.module('aapApp').controller('MainCtrl', function ($scope) {
 	$scope.dynamicPopoverText = "dynamic";
 	$scope.dynamicPopoverTitle = "Title";
 });
+
+angular.module('aapApp').controller('carousel', function ($scope) {
+	$scope.dynamicPopover = "Hello, World!";
+	$scope.dynamicPopoverText = "dynamic";
+	$scope.dynamicPopoverTitle = "Title";
+});
